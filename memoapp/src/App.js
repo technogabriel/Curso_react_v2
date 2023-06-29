a7d272801b35ea19ad6e965bad2809e600b988c8
adan.tarazona <adangabriel1@gmail.com>
2023-06-29 18:16:47 -0300

    memotest game v1

---
+++ memoapp/src/App.js
@@ -0,0 +1,14 @@
+
+import { MemoTest } from "./components/Memotest/MemoTest";
+
+
+
+function App() {
+  return (
+    <div className="container mt-3" >
+      <MemoTest/>
+    </div>
+  );
+}
+
+export default App;
